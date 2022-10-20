c6e901bfa5ea463ded68f5ac79400251069395d0
MaelPixel <113027326+MaelPixel@users.noreply.github.com>
2022-10-19 22:51:21 -0400

    exo avec exemple de .join() dans str

--- exercice.py
+++ exercice.py
@@ -4,21 +4,47 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	n=0
+	for c in text:
+
+		if c.isalnum():
+
+			n+=1
+
+	return n
 
 def get_word_length_histogram(text):
-	return [0]
+	liste=[]
+
+	for w in text.split():
+		l=get_num_letters(w)
+		if l>=len(liste):
+			liste+=[0]*(l-len(liste)+1)
+		liste[l]+=int(l!=0)
+	return liste
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
-
-	return ""
+	n=""
+	m=1
+	for i in range(len(histogram)):
+		print(i)
+		if i ==0:
+			continue
+		else:
+			n+='\n'+str(m)+'*'*(histogram[i])
+			m+=1
+	return n
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
-
-	return ""
+	hauteur=max(histogram)
+	str=""
+	for i in range(hauteur - 1, -1, -1):
+		str+="".join([BLOCK_CHAR if elem>=i+1 else ' ' for elem in histogram[1:]])+'\n'
+	str+=LINE_CHAR*len(histogram)
+	return str
 
 
 if __name__ == "__main__":
